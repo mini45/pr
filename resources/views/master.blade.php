9d819ca761b1b0ea1539803f18201901d02a87db
andré <andre.muenstermann@wabsolute.de>
2015-09-09 10:50:41 +0200

    added vieport

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -3,6 +3,7 @@
     <title>@yield('title')</title>
     <meta charset="UTF-8">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
     @section('styles')
         {!! HTML::style('css/app.css') !!}
         {!! HTML::style('css/jquery-ui.min.css') !!}
